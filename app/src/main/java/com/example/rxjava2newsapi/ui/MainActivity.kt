ed63788c9e3828c1f077fbad1dbdf110369e3cb9
Fahri MacBook <fahri-can@web.de>
2019-06-17 01:09:23 +0200

    Implement methods for RxJava.

--- app/src/main/java/com/example/rxjava2newsapi/ui/MainActivity.kt
+++ app/src/main/java/com/example/rxjava2newsapi/ui/MainActivity.kt
@@ -8,6 +8,7 @@ import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.SearchView
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
@@ -18,7 +19,10 @@ import com.example.rxjava2newsapi.model.TopHeadlines
 import com.example.rxjava2newsapi.news_api.TopHeadlinesEndpoint
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -67,7 +71,89 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (menu != null) {
+            val inflater: MenuInflater = menuInflater
+            inflater.inflate(R.menu.menu_main, menu)
+            //Creates input field for the user search
+            setUpSearchMenuItem(menu)
+        }
+        return true
+    }
 
+    private fun setUpSearchMenuItem(menu: Menu) {
+        val searchManager: SearchManager = (getSystemService(Context.SEARCH_SERVICE)) as SearchManager
+        val searchView: SearchView = ((menu.findItem(R.id.action_search)?.actionView)) as SearchView
+        val searchMenuItem: MenuItem = menu.findItem(R.id.action_search)
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = "Type any keyword to search..."
+        searchView.setOnQueryTextListener(onQueryTextListenerCallback())
+        searchMenuItem.icon.setVisible(false, false)
+    }
+
+    //Gets immediately triggered when user clicks on search icon and enters something
+    private fun onQueryTextListenerCallback(): SearchView.OnQueryTextListener {
+        return object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(userInput: String?): Boolean {
+                return checkQueryText(userInput)
+            }
+
+            override fun onQueryTextChange(userInput: String?): Boolean {
+                return checkQueryText(userInput)
+            }
+        }
+    }
+
+    private fun checkQueryText(queryText: String?): Boolean {
+        if (queryText != null && queryText.length > 1) {
+            userKeywordInput = queryText
+            getKeyWordQuery(queryText)
+        } else if (queryText != null || queryText == "") {
+            userKeywordInput = ""
+            queryTopHeadlines()
+        }
+        return false
+    }
+
+    private fun queryTopHeadlines() {
+        swipe_refresh.isRefreshing = true
+        topHeadlinesObservable = topHeadlinesEndpoint.getTopHeadlines("us", newsApiConfig)
+        getObservableOfArticle()
+    }
+
+    private fun getObservableOfArticle() {
+        articleList.clear()
+        compositeDisposable.add(
+            topHeadlinesObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap {
+                    Observable.fromArray(it.articles)
+                }
+                .flatMapIterable { listOfArticles ->
+                    listOfArticles
+                }
+                .subscribeWith(callbackArticleObserver())
+        )
+    }
+
+    private fun callbackArticleObserver(): DisposableObserver<Article> {
+        return object : DisposableObserver<Article>() {
+            override fun onNext(article: Article) {
+                if (!articleList.contains(article)) {
+                    articleList.add(article)
+                }
+            }
+
+            override fun onComplete() {
+                showArticlesOnRecyclerView()
+            }
+
+            override fun onError(e: Throwable) {
+                Log.e("callbackArticleObserver", "Article error: ${e.message}")
+            }
+        }
+    }
 
     private fun generateRetrofitBuilder(): Retrofit {
 
@@ -85,16 +171,6 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
         } else {
             getKeyWordQuery(userKeywordInput)
         }
-    }
-
-    private fun queryTopHeadlines() {
-        swipe_refresh.isRefreshing = true
-        topHeadlinesObservable = topHeadlinesEndpoint.getTopHeadlines("us", newsApiConfig)
-        getObservableOfArticle()
-    }
-
-    private fun getObservableOfArticle() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     private fun getKeyWordQuery(userKeywordInput: String) {
